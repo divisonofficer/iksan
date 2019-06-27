29f407aa14ead4e6a27d600d613ac24898881c71
KGH <show11837@gmail.com>
2019-06-28 00:56:56 +0900

    Province 이미지 처리

--- app/src/main/java/com/sss/fills/MainActivity.java
+++ app/src/main/java/com/sss/fills/MainActivity.java
@@ -1,8 +1,11 @@
 package com.sss.fills;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,6 +16,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -26,12 +31,23 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Button theButton = (Button)findViewById(R.id.SouthJeollaProvince);
+        theButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent1 = new Intent(MainActivity.this, ProvinceActivity.class);
+                startActivity(intent1);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/sss/fills/ProvinceActivity.java
@@ -0,0 +1,40 @@
+package com.sss.fills;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ProvinceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_province);
+
+        Button btnIcksan = (Button)findViewById(R.id.icksan);
+        btnIcksan.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent1 = new Intent(ProvinceActivity.this, MainActivity.class);
+                startActivity(intent1);
+            }
+        });
+
+        Button btnGunsan = (Button)findViewById(R.id.gunsan);
+        btnGunsan.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent1 = new Intent(ProvinceActivity.this, MainActivity.class);
+                startActivity(intent1);
+            }
+        });
+    }
+
+
+}
